55c34b897e0b6a8f15300f7619f504482af2492a
Cmiljaa <147316652+Cmiljaa@users.noreply.github.com>
2024-08-10 12:30:13 +0200

    Add 10 minutes caching for weather data

--- app/Services/WeatherService.php
+++ app/Services/WeatherService.php
@@ -2,19 +2,30 @@
 
 namespace App\Services;
 
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
 
 class WeatherService
 {
     public static function getForecast($city){
+
+        $cacheKey = "weather_{$city}";
+
+        if(Cache::has($cacheKey))
+        {
+            return Cache::get($cacheKey);
+        }
+
         $response = Http::get(env('WEATHER_API_URL'), [
             'q' => $city,
             'appid' => env('WEATHER_API_KEY'),
             'units' => 'metric'
         ]);
 
-        
+        $weatherApp = $response->json();
 
-        return $response->json();
+        Cache::put($cacheKey, $weatherApp, 10*60);
+
+        return $weatherApp;
     }
 }
